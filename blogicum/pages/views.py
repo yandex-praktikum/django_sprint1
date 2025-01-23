6f7a6a248be666811be10fb2b471a9e1d5783593
Zahar-Borovskiy <borovskiyzahar@gmail.com>
2025-01-23 13:49:14 -0300

    доделал pages (views)

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def about(request):
+    return render(request, 'pages/about.html')
+
+def rules(request):
+    return render(request, 'pages/rules.html')
