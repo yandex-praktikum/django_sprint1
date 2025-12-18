c4b0b5dffea4c491232e4991fb19add602a4c07d
Ягудин Данил <134008888+Spookich1337@users.noreply.github.com>
2025-12-18 07:31:31 +0300

    Add files via upload

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = "blog"
+category_name = "category/<slug:category_slug>/"
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts/<int:id>/", views.post_detail, name="post_detail"),
-    path("category/<slug:category_slug>/", views.category_posts, name="category_posts"),
+    path(category_name, views.category_posts, name="category_posts"),
 ]
