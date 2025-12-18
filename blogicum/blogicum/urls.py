100577cae8ea3ba944497c723596c905ac70404f
Ягудин Данил <134008888+Spookich1337@users.noreply.github.com>
2025-12-18 07:44:31 +0300

    Add files via upload

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,8 @@
-from django.urls import include, path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path("", include("blog.urls")),
-    path("pages/", include("pages.urls")),
+    path('admin/', admin.site.urls),
+    path('', include(('blog.urls', 'blog'), namespace='blog')),
+    path('pages/', include(('pages.urls', 'pages'), namespace='pages')),
 ]
